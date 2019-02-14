918b6d9b32bb2f842599f2158377c614a9907d07
griffinGC <griffinDouble@gmail.com>
2019-02-15 02:08:04 +0900

    test

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -8,7 +8,7 @@ class TodoList extends Component{
             <div>
                 <TodoItem done>리액트 공부하기!</TodoItem>
                 <TodoItem> 컴포넌트 스타일링!</TodoItem>
-
+{/* temp */}
             </div>
         )
     }
