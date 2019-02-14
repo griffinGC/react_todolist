fffed18370e6355eff063ca2b50b5fa416b660bb
griffinDouble <griffindouble@griffinDoubleui-MacBook-Pro.local>
2019-02-15 02:00:16 +0900

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import PageTemplate from './PageTemplate';
+import TodoInput from './TodoInput';
+import TodoList from './TodoList';
+
+class App extends Component {
+  render() {
+    return (
+      <PageTemplate>
+        <TodoInput/>
+        <TodoList></TodoList>
+      </PageTemplate>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/TodoInput/TodoInput.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from './TodoInput.scss';
+import classNames from 'classnames/bind';
+
+
+const cx = classNames.bind(styles);
+
+const TodoList = ({value, onChange, onInsert}) =>{
+
+    const handleKeyPress = (e) =>{
+        if(e.key === 'Enter'){
+            onInsert();
+        }
+    }
+
+    return (
+        <div className={cx('todo-input')}>
+            <input onChange={onChange} value ={value} onKeyPress={handleKeyPress}></input>
+            <div className={cx('add-button')} onClick={onInsert}>추가</div>
+        </div>
+    );
+
+};
+
+export default TodoList;
