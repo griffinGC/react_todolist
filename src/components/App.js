1ec276388ee798f8817e6a836b516e40d0d8bfe9
griffinGC <griffinDouble@gmail.com>
2019-04-01 10:45:51 +0900

    change

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,53 @@ import TodoInput from './TodoInput';
 import TodoList from './TodoList';
 
 class App extends Component {
+  state ={
+    input : '',
+    todos:[
+      {id : 0, text : "리액트 스터디!", done : true},
+      {id : 1, text : "node study", done : false},
+    ]
+  }
+
+  id = 1;
+  getId = () =>{
+    return ++this.id;
+  }
+
+  handleChange= (e) =>{
+    const {value} = e.target;
+    this.setState({
+      input : value
+    });
+  }
+
+  handleInsert = () =>{
+    const {todos, input} = this.state;
+
+    const newTodo ={
+      text : input,
+      done : false,
+      id : this.getId()
+    }
+
+    this.setState({
+      todos : [...todos, newTodo],
+      input : ''
+    });
+  }
+
+
   render() {
+    const {input, todos} = this.state;
+    const {
+      handleChange,
+      handleInsert
+    } = this;
+
     return (
       <PageTemplate>
-        <TodoInput/>
-        <TodoList></TodoList>
+        <TodoInput onChange={handleChange} value={input} onInsert={handleInsert}/>
+        <TodoList todos={todos}/>
       </PageTemplate>
     );
   }
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -4,11 +4,19 @@ import TodoItem from '../TodoItem';
 class TodoList extends Component{
 
     render(){
+        const {todos} = this.props;
+        const todoList = todos.map(
+            it => (
+                <TodoItem
+                    key = {it.id}
+                    done = {it.done} 
+                    >{it.text}
+                </TodoItem>
+            )
+        )
         return(
             <div>
-                <TodoItem done>리액트 공부하기!</TodoItem>
-                <TodoItem> 컴포넌트 스타일링!</TodoItem>
-{/* temp */}
+                {todoList}
             </div>
         )
     }
